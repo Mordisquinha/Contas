8c2a08a4f57f26d507cfcd530706f17a7b01f829
Gabriel Santiago <gabrielsantiago@MacBook-Air-de-Gabriel.local>
2023-02-14 22:10:03 -0300

    refact: putting value in var

--- receber_pix.py
+++ receber_pix.py
@@ -8,6 +8,7 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 nu = Nubank()
 cpf = os.getenv('CPF')
 senha = os.getenv('SENHA')
+valor = 0.01
 nu.authenticate_with_cert(cpf, senha, './cert.p12')
 
 data = nu.get_available_pix_keys()
@@ -20,7 +21,7 @@ for key in data['keys']:
 
 account_id = data['account_id'] # Retorna id da sua conta
 
-qr = nu.create_pix_payment_qrcode(account_id, 0.01, chave)
+qr = nu.create_pix_payment_qrcode(account_id, valor, chave)
 qr = qr['qr_code']
 img = qr.make_image(fill_color='black', back_color='white')
 
